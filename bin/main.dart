559e03fad23e42920ee7ffe053faafef902fd1d4
LAPTOP-KFFSGVHG\user <colonal.ch@gmail.com>
2024-08-18 21:41:47 +0300

    Respond with 200 #ia4

--- bin/main.dart
+++ bin/main.dart
@@ -5,5 +5,29 @@ void main() async {
 
   await for (var clientSocket in serverSocket) {
     print("Client connected");
+    print("Address: ${clientSocket.address.address}");
+    print("name: ${clientSocket.address.type.name}");
+    print("host: ${clientSocket.address.host}");
+
+    // Read the client's request data
+    List<int> data = await clientSocket.first;
+    String request = String.fromCharCodes(data);
+    print("Request: " + request.trim());
+
+    // response body
+    String responseBody = 'Hello, World!';
+
+    // Prepare the HTTP response
+    String response = 'HTTP/1.1 200 OK\r\n' +
+        'Content-Type: text/plain\r\n' +
+        'Content-Length: ${responseBody.length}\r\n' +
+        '\r\n' +
+        '$responseBody';
+
+    // Send the HTTP response back to the client
+    clientSocket.write(response);
+
+    // Close the connection
+    await clientSocket.close();
   }
 }
