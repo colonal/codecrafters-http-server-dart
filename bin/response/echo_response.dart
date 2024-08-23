d9e433481203a0165521e378ff37f97dc5e9ea07
LAPTOP-KFFSGVHG\user <colonal.ch@gmail.com>
2024-08-23 12:34:24 +0300

    Compression headers

--- bin/response/echo_response.dart
+++ bin/response/echo_response.dart
@@ -3,12 +3,24 @@ import '../model/request.dart';
 String echoResponse(Request request) {
   // response body
   String responseBody = request.requestLine.requestTarget.split("/echo/")[1];
+  String response = "";
 
-  // Prepare the HTTP response
-  String response = 'HTTP/1.1 200 OK\r\n' +
-      'Content-Type: text/plain\r\n' +
-      'Content-Length: ${responseBody.length}\r\n' +
-      '\r\n' +
-      '$responseBody';
+  if (request.headers['Accept-Encoding'] == 'gzip') {
+    // Prepare the HTTP response
+    response = 'HTTP/1.1 200 OK\r\n' +
+        'Content-Type: text/plain\r\n' +
+        'Content-Encoding: gzip\r\n' +
+        'Content-Length: ${responseBody.length}\r\n' +
+        '\r\n' +
+        '$responseBody';
+  } else {
+    // Prepare the HTTP response
+    response = 'HTTP/1.1 200 OK\r\n' +
+        'Content-Type: text/plain\r\n' +
+        'Content-Length: ${responseBody.length}\r\n' +
+        '\r\n' +
+        '$responseBody';
+  }
+
   return response;
 }
