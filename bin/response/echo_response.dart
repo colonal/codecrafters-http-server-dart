d96b0c38035d5f8e978e521dcd14118db9c2806d
LAPTOP-KFFSGVHG\user <colonal.ch@gmail.com>
2024-08-23 16:07:31 +0300

    Gzip compression

--- bin/response/echo_response.dart
+++ bin/response/echo_response.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:io';
+
 import '../model/request.dart';
 
 String echoResponse(Request request) {
@@ -9,13 +12,19 @@ String echoResponse(Request request) {
       request.headers['Accept-Encoding']?.replaceAll(' ', '').split(',') ?? [];
   print("acceptEncoding: $acceptEncoding");
   if (acceptEncoding.contains('gzip')) {
+    List<int> stringBytes = utf8.encode(responseBody);
+    List<int> stringGZip = GZipCodec().encode(stringBytes);
+
+    String body = base64.encode(stringGZip);
+    print("body: $body");
+
     // Prepare the HTTP response
     response = 'HTTP/1.1 200 OK\r\n' +
         'Content-Type: text/plain\r\n' +
         'Content-Encoding: gzip\r\n' +
-        'Content-Length: ${responseBody.length}\r\n' +
+        'Content-Length: ${body.length}\r\n' +
         '\r\n' +
-        '$responseBody';
+        '$body';
   } else {
     // Prepare the HTTP response
     response = 'HTTP/1.1 200 OK\r\n' +
